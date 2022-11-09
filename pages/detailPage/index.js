540dee6b57587f5be958e326b82834552cdca086
mr-vivek-verma <mr.vivek2090@gmail.com>
2022-11-09 17:46:41 +0530

    bugs fixed and added some more functionality

---
+++ pages/detailPage/index.js
@@ -0,0 +1,9 @@
+
+
+const index = () => {
+  return (
+    <div>this is detail page.</div>
+  )
+}
+
+export default index
--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,17 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import { GraphQLClient, gql  } from 'graphql-request';
+import Link from 'next/link';
+import { useState } from 'react';
 // import navbar from "../components/navbar/navbar"
 
+   
 
+
+
+
+                                                      
+                      
 export const getStaticProps = async () => {
   const endpoint = process.env.PREVIEW_CH_ENDPOINT;
   const graphQLClient = new GraphQLClient(endpoint);
@@ -19,15 +27,15 @@ export const getStaticProps = async () => {
   
   const query = gql`{
   hotelX {
-    search(
+    search(                                         
       criteria: { 
-        checkIn: "2022-11-01",
-        checkOut: "2022-11-06",
+        checkIn: "2022-11-09",
+        checkOut: "2022-11-10",
         occupancies: [{ paxes: [{age: 18}, {age: 30}] }],
         hotels: ["1"],
         currency: "EUR",
         market: "ES",
-        language: "es",
+        language: "es",        
         nationality: "ES"
       },
       settings: {
@@ -41,7 +49,7 @@ export const getStaticProps = async () => {
         access: {
           includes: ["0"]
         }
-      }) {
+      }) {                               
            context
       errors{
         code
@@ -237,17 +245,25 @@ export const getStaticProps = async () => {
 
 export default function ActionAreaCard({data}) {
    console.log("data", data.hotelX.search)
+   
+   const [hotel, SetHotel] = useState(false);
+  const handleclick = () => {
+    SetHotel(true);
+    
+    
+  }
   return (
     <> 
       <div>
              
         <h1>TravelX</h1>
-        </div>/
-      <div>{data && data.hotelX.search && data.hotelX.search.options.map((hotel) => {
+        </div>
+      <div>{data && data.hotelX.search && data.hotelX.search.options.map((tel) => {
+        
         return (
           <>  
           
-    <Card sx={{ maxWidth: 440,display:'inline-block' }}>
+    <Card sx={{ maxWidth: 440,display:'inline-block' }} >
       <CardActionArea sx={{display:'flex', justifyContent:"center", alignItems:"center"}}>
         <CardMedia
           component="img"
@@ -258,18 +274,24 @@ export default function ActionAreaCard({data}) {
         />
         <CardContent>
           <Typography gutterBottom variant="h6" component="div">
-          <h6 key={hotel.id}>Hotel Name: {hotel.hotelName}</h6>
+          <h6 key={tel.id}>Hotel Name: {tel.hotelName}</h6>
           </Typography>
-          <Typography variant="div" color="text.secondary">
-          <span>Payment Type: {hotel.paymentType}</span>
-          <div>RateRules: {hotel.rateRules}</div>
-          <div>currency: {hotel.price.currency}</div>
-           
-          </Typography>       
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  
+          <Typography variant="div" color="text.secondary"  onClick={(e) => {handleclick(e) }}>
+          
+          <span>Payment Type: {tel.paymentType}</span>
+          <div>RateRules: {tel.rateRules}</div>
+          <div>RateRules: {tel.rateRules ? tel.rateRules : "NA"}</div>
+          <div style={{ color: "#5E2125" }}>Currency: {tel.price.currency}</div>            
+          <div style={{margin:"10px"}}>    
+          <Link href="/detailPage">More Details...</Link>                             
+          </div>       
+          </Typography>                                                   
+        </CardContent>                                                                                
+                                                      
+      </CardActionArea>                                                                       
+                                                                                    
+      </Card>                                        
+       
        
           {/* <h4 key={hotel.id}>Hotel Name: {hotel.hotelName}</h4>
           <span>Payment Type: {hotel.paymentType}</span>
